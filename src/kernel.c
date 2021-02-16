50e8f5c3a1ec0fecb0debbea8b22742eb8727040
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2021-02-17 00:13:24 +0700

    Initial works on read/print string

--- src/kernel.c
+++ src/kernel.c
@@ -1,16 +1,62 @@
+#define bufsize 20
+
+void printString(char *string);
+void readString(char *string);
+void clear(char *buffer, int length);
+
 int main () {
-	char* HelloWorld = "Hello, world!";
-	int i = 0, j;
-	for(; i < 13; i++){
-		j = i<<1;
-		putInMemory(0xB000, 0x8000+j, HelloWorld[i]);
-		putInMemory(0xB000, 0x8001+j, 0xD);
-	}
+	char buffer[bufsize];
+	// set graphics mode
+	interrupt(0x10, 0x0004, 0x0000, 0x0000, 0x0000);
+	// gambar :D
+	interrupt(0x10, 0x0C0E, 0x0000, 5, 5);
 	makeInterrupt21();
-	interrupt(0x10, 0x0C0D, 0x0000, 0x0000, 0x0000); 
-	while (1);
+	while(1){
+		clear(buffer, bufsize);
+		interrupt(0x21, 0x01, buffer, 0x0000, 0x0000);
+		interrupt(0x21, 0x00, buffer, 0x0000, 0x0000);
+	}
 }
 
 void handleInterrupt21 (int AX, int BX, int CX, int DX) {
-	interrupt(0x10, AX, BX, CX, DX);
+	switch(AX){
+		case 0x0:
+			printString(BX);
+			break;
+		case 0x1:
+			readString(BX);
+			break;
+		default:
+			printString("invalid interrupt");
+	}
+}
+
+void printString(char *string){
+	int i = 0;
+	// set mode text 
+	interrupt(0x10, 0x0304, 0x0000, 0x0000, 0x0000);
+	for(i = 0; string[i] != 0; i++){
+		// set posisi kursor
+		interrupt(0x10, 0x0200, 0x0000, 0x0000, 0x0000+i);
+		// tulis string :D
+		interrupt(0x10, 0x0900 + string[i], 0x000D, 0x0001, 0x0000);
+	}
+}
+
+void readString(char *string){
+	int length = 0;
+	char current = interrupt(0x16, 0x0000, 0x0000, 0x0000, 0x0000);
+	while(length < bufsize-1 && current != 0x0D){
+		string[length] = current;
+		length++;
+		current = interrupt(0x16, 0x0000, 0x0000, 0x0000, 0x0000);
+	}
+	string[length] = 0;
+}
+
+void clear(char* buffer, int length){
+	int i;
+	for(i = 0; i < length; i++){
+		buffer[i] = 0;
+	}
 }
