f24a7f70698cfad26c90f3483bf444e208d9ad24
Hokki Suwanda <68459061+hokkyss@users.noreply.github.com>
2021-03-08 09:47:33 +0700

    Update cd.c

--- src/prog/cd.c
+++ src/prog/cd.c
@@ -2,7 +2,45 @@
 
 int chdir(const char* path)
 {
-	char *dummy;
+	char *cwd;
+	getcwd(cwd);
+	int curDirIdx = *cwd;
+	// kasus spesial :)
+	if(strcmp(args[i], "..") == 0)
+	{
+		if (path == NULL) return 1; // error
+		
+		// ke parent dari cwd
+		sector[511] = files[curDirIdx * 16];
+		return 0;
+	}
+		
+	if (cwd == NULL) // root
+	{
+		// cari yang nama filenya sama dengan args[i] dan parentIndexnya adalah 0xFF
+		for(j = 0; j <= 63; j++)
+		{
+			if(files[16 * j] == 0xFF && strcmp(strcpy(dummy, files[16 * j + 2], 14), args[i]) == 0)
+			{
+				sector[511] = files[16 * j];
+				return 0;
+			}
+		}
+	}
+	else
+	{
+		// cari yang nama filenya sama dengan args[i] dan parentIndexnya adalah cwd[0]
+		for(j = 0; j <= 63; j++)
+		{
+			if(files[16 * j] == *cwd && strcmp(strcpy(dummy, files[16 * j + 2], 14), args[i]) == 0)
+			{
+				sector[511] = files[16 * j];
+				return 0;
+			}
+		}
+	}
+	return 1;
+
 	/* ALTERNATIF 1 */
 	/*
 	int errno = interrupt(0x21, 0x3b02, dummy, 0x101, path);
@@ -19,51 +57,15 @@ int chdir(const char* path)
 
 int main(int argc, char *argv[])
 {
-	/* ALTERNATIF 2 */
 	char* args[255] = parse(argv[1], SLASH);
-	char* cwd;
 	
 	int i = 0, j;
 	char *dummy;
-	while (args[i] != NULL)
+	while (args[i] != NULL && i < 255)
 	{
-		getcwd(cwd);
-		int curDirIdx = *cwd;
-		// kasus spesial :)
-		if(strcmp(args[i], "..") == 0)
-		{
-			if (cwd == NULL); // error
-			
-			// ke parent dari cwd
-			sector[511] = files[curDirIdx * 16];
-			i++;
-			continue;
-		}
-		
-		if (cwd == NULL) // root
-		{
-			// cari yang nama filenya sama dengan args[i] dan parentIndexnya adalah 0xFF
-			for(j = 0; j <= 63; j++)
-			{
-				if(files[16 * j] == 0xFF && strcmp(strcpy(dummy, files[16 * j + 2], 14), args[i]) == 0)
-				{
-					sector[511] = files[16 * j];
-					break;
-				}
-			}
-		}
-		else
-		{
-			// cari yang nama filenya sama dengan args[i] dan parentIndexnya adalah cwd[0]
-			for(j = 0; j <= 63; j++)
-			{
-				if(files[16 * j] == *cwd && strcmp(strcpy(dummy, files[16 * j + 2], 14), args[i]) == 0)
-				{
-					sector[511] = files[16 * j];
-					break;
-				}
-			}
-		}
+		if(chdir(args[i]) == 1) return 1; // error??
+		// directorynya udah berubah karena chdir udah dipanggil
 		i++;
 	}
+	return 0;
 }
