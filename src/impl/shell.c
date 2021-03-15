0b6bec3eb728bbb43965802b7a403488e7cc9d81
cyn-rus <cyn.rusadi@gmail.com>
2021-03-15 22:55:01 +0700

    tambahin history, semoga bener

--- src/impl/shell.c
+++ src/impl/shell.c
@@ -3,59 +3,93 @@
 #include "progs.h"
 
 void runShell() {
-    char command[512], path, lsContent[32], changedPath;
-    int errNo;
+    char command[512], path, lsContent[32], changedPath, commandHistory[8];
+    int errNo, arrowClick, historyCount, historyIdx, i;
 
     path = 0xFF;
+    arrowClick = 0;
+    historyCount = 0;
+    historyIdx = -1;
     while (1) {
-        printString("[GanyuWangySekali]> ");
-        printPath(path);
+        if (arrowClick == 0) {
+            printString("[GanyuWangySekali]> ");
+            printPath(path);
+        }
+
         readString(command);
 
-        if (strncmp(command, "cd ", 3) == 1) {
-            changedPath = chdir(command+3, &errNo, path);
-            if (errNo == 1) {
-                printString("Not a directory");
+        if (command[0] == 0x00 && command[1] != 0x00) {
+            // ini ke atas yak
+            if (command[1] == 0x38) {
+                historyIdx--;
             }
-            else if (errNo == 2) {
-                printString("No such file or directory");
+            // ini ke bawah
+            else if (command[1] == 0x40) {
+                historyIdx++;
             }
-            else if (errNo == 3) {
-                printString("Not a valid file or directory");
-            }
-            else {
-                path = changedPath;
-            }
-        }
 
-        else if (strncmp(command, "ls", 2) == 1) {
-            ls(lsContent, path);
-            printString(lsContent);
-        }
-
-        else if (strncmp(command, "cat ", 4) == 1) {
-            cat(command+3 ,path);
-        }
-
-        else if (strncmp(command, "ln ", 3) == 1) {
-            if (strncmp(command+3, "-s ", 3)) {
-                // softln(command+6, , path);
+            if (historyIdx >= 0 && historyIdx < historyCount) {
+                printString(commandHistory[historyIdx]);
             }
-            else {
-                // ln (command+3, , path);
-            }
+
+            arrowClick = 1;
         }
 
         else {
-            printString("No command found");
+            arrowClick = 0;
+            if (strncmp(command, "cd ", 3) == 1) {
+                changedPath = chdir(command+3, &errNo, path);
+                if (errNo == 1) {
+                    printString("Not a directory");
+                }
+                else if (errNo == 2) {
+                    printString("No such file or directory");
+                }
+                else if (errNo == 3) {
+                    printString("Not a valid file or directory");
+                }
+                else {
+                    path = changedPath;
+                }
+            }
+
+            else if (strncmp(command, "ls", 2) == 1) {
+                ls(lsContent, path);
+                printString(lsContent);
+            }
+
+            else if (strncmp(command, "cat ", 4) == 1) {
+                cat(command+3 ,path);
+            }
+
+            else if (strncmp(command, "ln ", 3) == 1) {
+                if (strncmp(command+3, "-s ", 3)) {
+                    // softln(command+6, , path);
+                }
+                else {
+                    // ln (command+3, , path);
+                }
+            }
+
+            else {
+                printString("No command found");
+            }
+            printString("\n");
+
+            // simpen history
+            for (i = historyCount-1; i > 0; i--) {
+                strcpy(commandHistory[i], commandHistory[i-1]);
+            }
+            strcpy(commandHistory[historyCount], command);
+            historyCount++;
+            historyIdx = historyCount-1;
         }
-        printString("\n");
     }
 }
 
 void printPath(char path) {
     char order[32], dir[1024], filename[14], currDir[32];
-    int numOfOrder, j;
+    int numOfOrder, j, i;
 
     printString("~");
     if (path != 0xFF) {
@@ -69,7 +103,7 @@ void printPath(char path) {
             numOfOrder++;
         }
 
-        for (int i = numOfOrder-1; i >= 0; i--) {
+        for (i = numOfOrder-1; i >= 0; i--) {
             j = numOfOrder-1-i;
             currDir[j] = order[i];
 
