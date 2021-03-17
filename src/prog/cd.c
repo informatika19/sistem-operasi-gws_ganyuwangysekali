ed9fbc2883a571b2bb4915263c04124b8756e539
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2021-03-17 11:41:37 +0700

    Add case for root

--- src/prog/cd.c
+++ src/prog/cd.c
@@ -14,21 +14,23 @@ char chdir(char* inputPath, int* result, char parentIndex)
 	}
 	
 	pathIndex = getPathIndex(inputPath, parentIndex);
-	
+
 	if(pathIndex == 0xFE)
 	{
 		*result = 2;
 		return parentIndex;
+	}
+
+	if(pathIndex == 0xFF){
+		*result = 0;
+		return 0xFF;
 	}
 	
 	readSector(dir, 0x101);
 	readSector(dir + 512, 0x102);
 	
 	// kasus softlink
-	while(dir[pathIndex << 4 + 1] >= 0x40)
-	{
-		pathIndex = dir[pathIndex << 4 + 1] - 0x40;
-	}
+	if(dir[(pathIndex<<4)+1] > 0x1F && dir[(pathIndex<<4)+1] != 0xFF) pathIndex = dir[(pathIndex << 4) + 1] - 0x20;
 	
 	// sectornya itu sector file
 	if ((dir[(pathIndex << 4) + 1] <= 0x1F) && (dir[(pathIndex << 4) + 1] >= 0x00))
