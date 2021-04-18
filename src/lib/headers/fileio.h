8f6def8bc2cbb537f59e8fa0186a4477e914a756
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2021-04-18 16:30:45 +0700

    Fix signature for exec

--- src/lib/headers/fileio.h
+++ src/lib/headers/fileio.h
@@ -12,6 +12,6 @@ void lib_readFile(char* buffer, char* name, int* err, char parent);
 void lib_readSector(char *buffer, int sector);
 void lib_writeSector(char *buffer, int sector);
 void removeIndex(char index, int* errno, char** files, char** sectors, char** maps);
-void exec(char* name, char parent, int* err, int segment);
+void exec(char* name, char parent, int* err);
 
 #endif
